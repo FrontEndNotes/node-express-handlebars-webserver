a954dc7613c0a8daa018cd2719528695df947857
P.K <pk@example.com>
2017-08-31 06:44:47 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const hbs = require('hbs'); //handlebars
+const fs = require('fs');
+
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+// set view engine as handlebars
+app.set('view engine', 'hbs');
+
+
+// next - tells express that middleware function is done
+app.use((req, res, next)=>{
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err)=> {
+        console.log('Unable to append to server log');
+    });
+
+    next();
+});
+
+
+// next - tells express that middleware function is done
+// app.use((req, res, next)=>{
+//     res.render('maintenance.hbs') 
+// });
+
+
+// app.use - middleware
+// express.static() - serve a folder (absolute path)
+// __dirname - path to project's directory
+// http://localhost:3000/help.html
+app.use(express.static(__dirname + '/public')); 
+
+
+hbs.registerHelper('getCurrentYear', ()=>{
+    return new Date().getFullYear()
+});
+
+
+
+app.get('/', (req, res) => {
+    //res.send('hello world!');  
+    
+    //render - render a page based on a template
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website!'
+    })   
+});
+
+
+app.get('/json', (req, res) => {
+    res.send({
+        title: "note title",
+        body: "note body"
+    });    
+});
+
+
+// http://localhost:3000/about
+app.get('/about', (req, res) => {
+
+    //render - render a page based on a template
+    res.render('about.hbs', {
+        pageTitle: 'About page'
+    })    
+});
+
+
+app.listen(3000, ()=>{
+    console.log('Server is up on port 3000');
+});
